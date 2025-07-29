65ea2ea90a37665ee34cfbbb803e66242a6dc9c8
Dharmendra7878 <djaat1426@gmail.com>
2025-07-29 09:39:00 +0530

    Update Code

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Valentine-Day-project/", // name of your repo
   plugins: [react()],
-})
+});
+
