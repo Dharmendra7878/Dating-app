4692a93b471bcaecb4da4021c9aba419dc16d765
Dharmendra7878 <djaat1426@gmail.com>
2025-07-29 11:29:38 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/Dating_project/', // name of your repo
+    base: '/Dating-app/', // name of your repo
   plugins: [react()],
 });
 
